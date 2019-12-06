c1c41fc7b0e8d754177b59c06ce16f4084212f9a
Carleigh Crockett <capleugh@gmail.com>
2019-12-06 10:39:44 -0700

    Add course index route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   get '/students', to: 'students#index'
   get '/students/:id', to: 'students#show'
+
+  get '/courses', to: 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
